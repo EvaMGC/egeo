219182bc332bf17039f9c3d184647a5a5ad8339d
EvaMGC <emgaitan@stratio.com>
2018-01-18 13:58:59 +0100

    feat(st-widget)[NA]: Fix lint errors

--- src/lib/tests/utils.ts
+++ src/lib/tests/utils.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 export class TestUtils {
 
-   static overrideComponentTemplate( componentClass: any, customTemplate?: string,): Promise<ComponentFixture<any>> {
+   static overrideComponentTemplate(componentClass: any, customTemplate?: string): Promise<ComponentFixture<any>> {
       if (customTemplate) {
          TestBed.overrideComponent(componentClass, {
             set: {
